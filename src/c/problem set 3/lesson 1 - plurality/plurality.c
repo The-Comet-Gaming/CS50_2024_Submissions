445d63e06b057598ac6c4ec4e1372874194bfdf6
Tristan Stewart <tjstew880@gmail.com>
2024-04-27 15:29:19 +1000

    Pushing uncommited changes made to pluraliity.c last night. 1. Implemented the "bool vote(string name)" in a working state. 2. Began puesdo-coding and brainstorming methods for the "void print winner(int voterCount)" function.

--- src/c/problem set 3/lesson 1 - plurality/plurality.c
+++ src/c/problem set 3/lesson 1 - plurality/plurality.c
@@ -20,7 +20,7 @@ int candidate_count;
 
 // Function prototypes
 bool vote(string name);
-void print_winner(void);
+void print_winner(int voterCount);
 
 int main(int argc, string argv[])
 {
@@ -59,18 +59,18 @@ int main(int argc, string argv[])
     }
 
     // Display winner of election
-    print_winner();
+    print_winner(voter_count);
 }
 
 // Update vote totals given a new vote
 bool vote(string name)
 {
-    // TODO
-    for (int loop = 0; loop < candidate_count; ++loop)
+    //printf("Input Being Checked: %s\n", name);
+    for (int loopCount = 0; loopCount < candidate_count; ++loopCount) // Iterate over each candidate
     {
-        if (candidate[i].name == name)
+        if (strcmp(candidates[loopCount].name, name) == 0)
         {
-            ++candidate[i].votes;
+            ++candidates[loopCount].votes;
             return true;
         }
     }
@@ -78,9 +78,52 @@ bool vote(string name)
 }
 
 // Print the winner (or winners) of the election
-void print_winner(void)
+void print_winner(int voterCount)
 {
+    //int maxVotesNeeded = voterCount / candidate_count;
+    //if (maxVotesNeeded < 1)
+    //{
+        //maxVotesNeeded = 1;
+    //}
+    //for (int loopCount = 0; loopCount < candidate_count; ++loopCount)
+    //{
+        //if (candidates[loopCount].votes == maxVotesNeeded)
+        //{
+            //printf("%s\n", candidates[loopCount].name);
+        //}
+    //}
     // TODO
-    // print candidate[i].name 
+    // take the number of voters and divide by the number of candidates
+    // initialize an int of number of votes needed to win, take the number of voters and divide it by the number of candidates,
+    // unless it is less than 1 that number is the max number of votes needed to win
+    // loop through each candidate and print them off if they have the correct number of votes
+
+    // have an int that is set to 1, being the minimum number of votes
+
+
+    // function takes int argument (which is the number of voters)
+
+
+
+    // algorithim  that goes through the candidates[].votes
+
+        // for loop equal to candidate count, selects the candidate[1stLoop].vote that is being reassigned
+
+            // intialize an int called currentHighestVote to equal 0;
+            // intialize an int called lowerValueIndex to equal 0;
+            // intialize an int called higherValueIndex to equal 0;
+
+            // for loop again equal to candidate count, goes through the candidare[2ndLoop].vote indexes
+
+                // if the index of the 1st loop is equal to the index of the 2nd loop then skip over the check
+
+                // if the candidates[loop2nd].votes is > candidates[loop1st].votes
+
+                    // set higherValueIndex = loop2nd;
+
+        // it takes the value of candidates[].votes and compares it too currentHighestVote;
+
+    // intialize an int that is the number of voters / 2, call it voteMajority
+
     return;
 }
