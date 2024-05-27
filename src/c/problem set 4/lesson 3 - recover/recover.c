dfc6fce11349420638c1a1c4e1cf3ef18409b1a8
Tristan Stewart <tjstew880@gmail.com>
2024-05-27 20:31:13 +1000

    added fully working and final version of helper.c for filter-more, and implemented in progress work on recover.c

--- src/c/problem set 4/lesson 3 - recover/recover.c
+++ src/c/problem set 4/lesson 3 - recover/recover.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
-int main(int argc, char *argv[])
+
+const int BlockSize = 512;
+
+int main(int numberOfArguments, char *cardName[])
 {
- 
+    if (numberOfArguments != 2)
+    {
+        printf("Usage: ./recover FILE\n");
+        return 1;
+    }
+
+    FILE *card = fopen(cardName[1], "r");
+    if (card == NULL)
+    {
+        printf("Could not open %p.\n", card);
+        return 1;
+    }
+
+    uint8_t buffer[BlockSize];
+
+    // While there's still data left to read from the memory card
+    while (fread(buffer, 1, BlockSize, card) == 512)
+    {
+        // Create JPEGs from the data
+
+        // check if signiture bytes are correct / present
+    }
+
+    fclose(card);
+    return 0;
 }
