26a0c69d20ee1a9efdbd95dd80bbb66a4bcb1033
Tj Stewart <tjstew880@gmail.com>
2024-06-01 00:09:05 +1000

    Adding recover.c + others from Set 4 (#12)
    * added fully working and final version of helper.c for filter-more, and implemented in progress work on recover.c
    
    * this version causes a segentation fault which is still to be indentified.
    
    * finished and uploaded lessons for submission!

--- src/c/problem set 4/lesson 2 - filter-more/helpers.c
+++ src/c/problem set 4/lesson 2 - filter-more/helpers.c
@@ -1,7 +1,7 @@
 #include "helpers.h"
 #include <math.h>
 
-int SetWithinRange(double value);
+int BinaryOffset = 1;
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
@@ -26,24 +26,17 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     RGBTRIPLE buffer;
-    int binaryOffset = 1;
     for (int i = 0; i < height; ++i)
     {
         for (int j = 0; j < width / 2; ++j)
         {
-            int oppositeIndex = width - binaryOffset - j;
+            int oppositeIndex = width - BinaryOffset - j;
 
-            buffer.rgbtRed = image[i][j].rgbtRed;
-            buffer.rgbtGreen = image[i][j].rgbtGreen;
-            buffer.rgbtBlue = image[i][j].rgbtBlue;
+            buffer = image[i][j];
 
-            image[i][j].rgbtRed = image[i][oppositeIndex].rgbtRed;
-            image[i][j].rgbtGreen = image[i][oppositeIndex].rgbtGreen;
-            image[i][j].rgbtBlue = image[i][oppositeIndex].rgbtBlue;
+            image[i][j] = image[i][oppositeIndex];
 
-            image[i][oppositeIndex].rgbtRed = buffer.rgbtRed;
-            image[i][oppositeIndex].rgbtGreen = buffer.rgbtGreen;
-            image[i][oppositeIndex].rgbtBlue = buffer.rgbtBlue;
+            image[i][oppositeIndex] = buffer;
         }
     }
     return;
@@ -57,9 +50,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; ++j)
         {
-            copyImage[i][j].rgbtRed = image[i][j].rgbtRed;
-            copyImage[i][j].rgbtGreen = image[i][j].rgbtGreen;
-            copyImage[i][j].rgbtBlue = image[i][j].rgbtBlue;
+            copyImage[i][j] = image[i][j];
         }
     }
 
@@ -68,42 +59,29 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; ++j)
         {
             float divider = 0.0;
-            double averageRed = 0.0;
-            double averageGreen = 0.0;
-            double averageBlue = 0.0;
-            int rowOffset = -1;
-            int pixelOffset = -1;
-            for (int k = 0; k < 9; ++k)
+            float averageRed = 0.0;
+            float averageGreen = 0.0;
+            float averageBlue = 0.0;
+            for (int row = -1; row <= 1; row++)
             {
-                if (k == 3 || k == 6)
+                int offsetRow = i + row;
+                for (int column = -1; column <= 1; column++)
                 {
-                    ++rowOffset;
-                    pixelOffset = -1;
+                    int offsetColumn = j + column;
+                    if (offsetRow >= 0 && offsetRow < height && offsetColumn >= 0 &&
+                        offsetColumn < width)
+                    {
+                        divider++;
+                        averageRed = averageRed + copyImage[offsetRow][offsetColumn].rgbtRed;
+                        averageGreen = averageGreen + copyImage[offsetRow][offsetColumn].rgbtGreen;
+                        averageBlue = averageBlue + copyImage[offsetRow][offsetColumn].rgbtBlue;
+                    }
                 }
-
-                if (i + rowOffset < 0 || i + rowOffset > height - 1)
-                {
-                    continue;
-                }
-                if (j + pixelOffset < 0 || j + pixelOffset > width - 1)
-                {
-                    ++pixelOffset;
-                    continue;
-                }
-
-                ++divider;
-                averageRed = averageRed + copyImage[i + rowOffset][j + pixelOffset].rgbtRed;
-                averageGreen = averageGreen + copyImage[i + rowOffset][j + pixelOffset].rgbtGreen;
-                averageBlue = averageBlue + copyImage[i + rowOffset][j + pixelOffset].rgbtBlue;
-                ++pixelOffset;
             }
-            averageRed /= divider;
-            averageGreen /= divider;
-            averageBlue /= divider;
 
-            image[i][j].rgbtRed = round(averageRed);
-            image[i][j].rgbtGreen = round(averageGreen);
-            image[i][j].rgbtBlue = round(averageBlue);
+            image[i][j].rgbtRed = round(averageRed / divider);
+            image[i][j].rgbtGreen = round(averageGreen / divider);
+            image[i][j].rgbtBlue = round(averageBlue / divider);
         }
     }
     return;
@@ -113,100 +91,68 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
     RGBTRIPLE copyImage[height][width];
-    for (int i = 0; i < height; ++i)
+    int gx[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+
+    int gy[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
+
+    for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < width; ++j)
+        for (int j = 0; j < width; j++)
         {
-            copyImage[i][j].rgbtRed = image[i][j].rgbtRed;
-            copyImage[i][j].rgbtGreen = image[i][j].rgbtGreen;
-            copyImage[i][j].rgbtBlue = image[i][j].rgbtBlue;
+            int gxSumRed = 0;
+            int gxSumGreen = 0;
+            int gxSumBlue = 0;
+
+            int gySumRed = 0;
+            int gySumGreen = 0;
+            int gySumBlue = 0;
+
+            for (int row = -1; row <= 1; row++)
+            {
+                for (int column = -1; column <= 1; column++)
+                {
+                    int neighbourRow = i + row;
+                    int neighbourColumn = j + column;
+
+                    if (neighbourRow >= 0 && neighbourRow < height && neighbourColumn >= 0 &&
+                        neighbourColumn < width)
+                    {
+                        gxSumRed +=
+                            image[neighbourRow][neighbourColumn].rgbtRed * gx[row + 1][column + 1];
+
+                        gxSumGreen += image[neighbourRow][neighbourColumn].rgbtGreen *
+                                      gx[row + 1][column + 1];
+
+                        gxSumBlue +=
+                            image[neighbourRow][neighbourColumn].rgbtBlue * gx[row + 1][column + 1];
+
+                        gySumRed +=
+                            image[neighbourRow][neighbourColumn].rgbtRed * gy[row + 1][column + 1];
+
+                        gySumGreen += image[neighbourRow][neighbourColumn].rgbtGreen *
+                                      gy[row + 1][column + 1];
+
+                        gySumBlue +=
+                            image[neighbourRow][neighbourColumn].rgbtBlue * gy[row + 1][column + 1];
+                    }
+                }
+            }
+            int sumRed = round(sqrt(gxSumRed * gxSumRed + gySumRed * gySumRed));
+            int sumGreen = round(sqrt(gxSumGreen * gxSumGreen + gySumGreen * gySumGreen));
+            int sumBlue = round(sqrt(gxSumBlue * gxSumBlue + gySumBlue * gySumBlue));
+
+            copyImage[i][j].rgbtRed = sumRed > 255 ? 255 : sumRed;
+            copyImage[i][j].rgbtGreen = sumGreen > 255 ? 255 : sumGreen;
+            copyImage[i][j].rgbtBlue = sumBlue > 255 ? 255 : sumBlue;
         }
     }
 
-    int gridSize = 9;
-    int Gx[gridSize];
-    Gx[0] = -1;
-    Gx[1] = 0;
-    Gx[2] = 1;
-    Gx[3] = -2;
-    Gx[4] = 0;
-    Gx[5] = 2;
-    Gx[6] = -1;
-    Gx[7] = 0;
-    Gx[8] = 1;
-
-    int Gy[gridSize];
-    Gy[0] = -1;
-    Gy[1] = -2;
-    Gy[2] = -1;
-    Gy[3] = 0;
-    Gy[4] = 0;
-    Gy[5] = 0;
-    Gy[6] = 1;
-    Gy[7] = 2;
-    Gy[8] = 1;
-
-    for (int i = 0; i < height; ++i)
+    for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < width; ++j)
+        for (int j = 0; j < width; j++)
         {
-            int rowOffset = -1;
-            int pixelOffset = -1;
-
-            long GxSumRed = 0;
-            long GxSumGreen = 0;
-            long GxSumBlue = 0;
-
-            long GySumRed = 0;
-            long GySumGreen = 0;
-            long GySumBlue = 0;
-            for (int k = 0; k < gridSize; ++k)
-            {
-                if (k == 3 || k == 6)
-                {
-                    ++rowOffset;
-                    pixelOffset = -1;
-                }
-                if (i + rowOffset < 0 || i + rowOffset > height - 1)
-                {
-                    continue;
-                }
-                if (j + pixelOffset < 0 || j + pixelOffset > width - 1)
-                {
-                    ++pixelOffset;
-                    continue;
-                }
-                GxSumRed += copyImage[i + rowOffset][j + pixelOffset].rgbtRed * Gx[k];
-                GxSumGreen += copyImage[i + rowOffset][j + pixelOffset].rgbtGreen * Gx[k];
-                GxSumBlue += copyImage[i + rowOffset][j + pixelOffset].rgbtBlue * Gx[k];
-
-                GySumRed += copyImage[i + rowOffset][j + pixelOffset].rgbtRed * Gy[k];
-                GySumGreen += copyImage[i + rowOffset][j + pixelOffset].rgbtGreen * Gy[k];
-                GySumBlue += copyImage[i + rowOffset][j + pixelOffset].rgbtBlue * Gy[k];
-            }
-            double sumRed = GxSumRed * GxSumRed + GySumRed * GySumRed;
-            double sumGreen = GxSumGreen * GxSumGreen + GySumGreen * GySumGreen;
-            double sumBlue = GxSumBlue * GxSumBlue + GySumBlue * GySumBlue;
-
-            image[i][j].rgbtRed = SetWithinRange(sumRed);
-            image[i][j].rgbtGreen = SetWithinRange(sumGreen);
-            image[i][j].rgbtBlue = SetWithinRange(sumBlue);
+            image[i][j] = copyImage[i][j];
         }
     }
     return;
-}
-
-int SetWithinRange(double value)
-{
-    value = sqrt(value);
-    value = round(value);
-    if (value < 0)
-    {
-        return 0;
-    }
-    if (value > 255)
-    {
-        return 255;
-    }
-    return value;
 }
--- src/c/problem set 4/lesson 3 - recover/recover.c
+++ src/c/problem set 4/lesson 3 - recover/recover.c
@@ -1,7 +1,64 @@
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
- 
-int main(int argc, char *argv[])
+
+int main(int numberOfArguments, char *cardName[])
 {
- 
+    if (numberOfArguments != 2)
+    {
+        printf("Usage: ./recover FILE\n");
+        return 1;
+    }
+
+    FILE *card = fopen(cardName[1], "r");
+    if (card == NULL)
+    {
+        printf("Could not open %p.\n", card);
+        return 1;
+    }
+
+    const int blockSize = 512;
+    uint8_t buffer[blockSize];
+    int header[5] = {0xff, 0xd8, 0xff, 0xf0, 0xe0};
+
+    char jpgName[8] = "000.jpg";
+    int jpgsWritten = 0;
+    FILE *jpg;
+
+    bool writingToFile = false;
+
+    // loop while there is still more read from the card
+    while (fread(buffer, 1, blockSize, card) == blockSize)
+    {
+        // check if the block has the headerBytes
+        if (buffer[0] == header[0] && buffer[1] == header[1] && buffer[2] == header[2] &&
+            (buffer[3] & header[3]) == header[4])
+        {
+            // close a jpg before writing to a new one
+            if (writingToFile == true)
+            {
+                fclose(jpg);
+            }
+
+            // initialize the header name
+            // increase a variable for the .jpg name +1
+            sprintf(jpgName, "%03i.jpg", jpgsWritten);
+            jpgsWritten++;
+
+            // create a new .jpg
+            jpg = fopen(jpgName, "a");
+
+            // set bool that says if we're writing to a file as true
+            writingToFile = true;
+        }
+
+        if (writingToFile == true)
+        {
+            fwrite(buffer, sizeof(buffer), 1, jpg);
+        }
+    }
+    fclose(jpg);
+    fclose(card);
+    return 0;
 }
