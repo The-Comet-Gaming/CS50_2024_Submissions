139b936c4b8cc59dfc8f72a7d1fada2002fd7f37
The-Comet-Gaming <tjstew880@gmail.com>
2024-06-20 19:27:04 +1000

    implemented more features into dictionary.c, all features are untested: load, unload, hash. Currently in progress: check, size

--- src/c/problem set 5/lesson 1 - speller/dictionary.c
+++ src/c/problem set 5/lesson 1 - speller/dictionary.c
@@ -1,7 +1,10 @@
 // Implements a dictionary's functionality
 
 #include <ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 #include "dictionary.h"
 
@@ -14,6 +17,7 @@ typedef struct node
 
 // TODO: Choose number of buckets in hash table
 const unsigned int N = 26;
+unsigned int WordsLoaded = 0;
 
 // Hash table
 node *table[N];
@@ -22,6 +26,21 @@ node *table[N];
 bool check(const char *word)
 {
     // TODO
+    // hash the word
+    int hashNumber = hash(word);
+
+    // search the hash node linked list on the table
+    for (node *ptr = table[hashNumber]; ptr != NULL; ptr = ptr->next)
+    {
+        // check if string lengths are equal
+        if (strlen(word) == strlen(ptr->word))
+        {
+            //
+        }
+        // convert to lower case before comparing
+        // check if strings are equal
+    }
+
     return false;
 }
 
@@ -38,24 +57,69 @@ bool load(const char *dictionary)
     // TODO
     // Open the dictionary file
     FILE *source = fopen(dictionary, "r");
-    // check if file opened correctly
     if (source == NULL)
     {
-        printf("Could not open %p.\n", source);
+        //printf("Could not open %p.\n", source);
         return false;
     }
 
+    char *word = NULL;
+
     // Read each word in the file
+    while (fscanf(source, "%s", word) != EOF)
+    {
+        // Create space for a new hash table node
+        node *n = malloc(sizeof(node));
+        if (n == NULL)
+        {
+            return false;
+        }
 
-        // Add each word to the hash table
+        WordsLoaded++;
 
-            // Create space for a new hash table node
+        // Copy the word into the new node
+        strcpy(n->word, word);
+        n->next = NULL;
 
-            // Copy the word into the new node
+        // Hash the word to obtain its hash value
+        int hashNumber = hash(word);
 
-            // Hash the word to obtain its hash value
-
-            // Insert the new node into the hash table (using the index specified by its hash value)
+        // Insert the new node into the hash table (using the index specified by its hash value)
+        // If the table is empty
+        if (table[hashNumber] == NULL)
+        {
+            table[hashNumber] = n;
+        }
+        // If number belongs at beginning of list
+        else if (strcmp(n->word, table[hashNumber]->word) < 0)
+        {
+            n->next = table[hashNumber];
+            table[hashNumber] = n;
+        }
+        // If number belongs later in list
+        else
+        {
+            for (node *ptr = table[hashNumber]; ptr != NULL; ptr = ptr->next)
+            {
+                // we need to check if the n word (oh dear) comes before table[N]->word
+                // if it does then we need to insert the node into the chain
+                // If at end of list
+                if (ptr->next == NULL)
+                {
+                    // Append node
+                    ptr->next = n;
+                    break;
+                }
+                // If in middle of list
+                if (strcmp(n->word, ptr->next->word) < 0)
+                {
+                    n->next = ptr->next;
+                    ptr->next = n;
+                    break;
+                }
+            }
+        }
+    }
 
     // Close the dictionary file
     fclose(source);
@@ -66,12 +130,41 @@ bool load(const char *dictionary)
 unsigned int size(void)
 {
     // TODO
-    return 0;
+    return WordsLoaded;
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
     // TODO
-    return false;
+    node *cursor = malloc(sizeof(node));
+    if (cursor == NULL)
+    {
+        return false;
+    }
+    node *tmp = malloc(sizeof(node));
+    if (tmp == NULL)
+    {
+        free(cursor);
+        return false;
+    }
+
+    // iterate through the table
+    for (int i = 0; i < N; i++)
+    {
+        // point cursor and tmp to the first node in the list of i
+        cursor = table[i];
+        tmp = table[i];
+
+        // iterate through the linked list
+        while (tmp != NULL)
+        {
+            cursor = table[i]->next;
+            free(tmp);
+            tmp = cursor;
+        }
+    }
+    free(cursor);
+    free(tmp);
+    return true;
 }
