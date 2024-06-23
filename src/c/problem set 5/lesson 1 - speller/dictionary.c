09e0ca02fc3c9f50c0f6a26907c82c93e0800f0e
Tj Stewart <tjstew880@gmail.com>
2024-06-23 19:31:28 +1000

    Implementing speller code (#17)
    * Began working again, woo! Implemented intial puesdo-code for the load function in dictionary.c
    
    * implemented more features into dictionary.c, all features are untested: load, unload, hash. Currently in progress: check, size
    
    * First fully working version of the speller code. Currently runs at 1.5 seconds total time. Will work on differing approaches to push down the real run time.
    
    * Second fully working version of speller. Run time is down to half a second on average.
    
    * Final version of speller. Fully working, run time is down to benchmark avg. 0.05/6 seconds. Submitted.
    
    * fixed indentation error on lines 121-123

--- src/c/problem set 5/lesson 1 - speller/dictionary.c
+++ src/c/problem set 5/lesson 1 - speller/dictionary.c
@@ -2,6 +2,10 @@
 
 #include <ctype.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
 
 #include "dictionary.h"
 
@@ -13,7 +17,9 @@ typedef struct node
 } node;
 
 // TODO: Choose number of buckets in hash table
-const unsigned int N = 26;
+const unsigned int N = 5393;
+
+int WordsLoaded = 0;
 
 // Hash table
 node *table[N];
@@ -22,6 +28,18 @@ node *table[N];
 bool check(const char *word)
 {
     // TODO
+    for (node *ptr = table[hash(word)]; ptr != NULL; ptr = ptr->next)
+    {
+        int i = strcasecmp(word, ptr->word);
+        if (i < 0)
+        {
+            return false;
+        }
+        if (i == 0)
+        {
+            return true;
+        }
+    }
     return false;
 }
 
@@ -29,26 +47,80 @@ bool check(const char *word)
 unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
-    return toupper(word[0]) - 'A';
+    int hashNumber = 0;
+    int n = strlen(word);
+    for (int i = 0; i < n; i++)
+    {
+        if (word[i] >= 'A' && word[i] <= 'Z')
+        {
+            hashNumber += tolower(word[i]);
+        }
+        else
+        {
+            hashNumber += word[i];
+        }
+    }
+    return hashNumber - 'a';
 }
 
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
     // TODO
-    return false;
+    // Open the dictionary file
+    FILE *source = fopen(dictionary, "r");
+    if (source == NULL)
+    {
+        return false;
+    }
+
+    char loadedWord[LENGTH + 1];
+
+    // Read each word in the file
+    while (fscanf(source, "%s", loadedWord) != EOF)
+    {
+        // Create space for a new hash table node
+        node *n = malloc(sizeof(node));
+        if (n == NULL)
+        {
+            fclose(source);
+            return false;
+        }
+
+        WordsLoaded++;
+
+        // Copy the word into the new node and set link pointer to NULL
+        strcpy(n->word, loadedWord);
+        // Hash the word to obtain its hash value
+        int i = hash(n->word);
+        n->next = table[i];
+        table[i] = n;
+    }
+    // Close the dictionary file
+    fclose(source);
+    return true;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
     // TODO
-    return 0;
+    return WordsLoaded;
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
     // TODO
-    return false;
+    for (int i = 0; i < N; i++)
+    {
+        node *cursor = table[i];
+        for (node *tmp = table[i]; tmp != NULL; tmp = cursor)
+        {
+            cursor = cursor->next;
+            free(tmp);
+            tmp = cursor;
+        }
+    }
+    return true;
 }
