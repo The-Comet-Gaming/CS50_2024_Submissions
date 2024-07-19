fd04cfaa606f9b08d487769cce94d659140f874c
The-Comet-Gaming <tjstew880@gmail.com>
2024-07-19 21:24:39 +1000

    Wrote some psuedo-code to help give some better direction for implementing a working piece of code

--- src/python/problem set 6/lesson 4 - sentimental-credit/credit.py
+++ src/python/problem set 6/lesson 4 - sentimental-credit/credit.py
@@ -1,16 +1,34 @@
 # # do it as a str and iterate over it cast each number to an int for the math
 
+# PSUEDO-CODE
+# Get user to input card number to a str
+
+# get length of card string
+# intialise variable for the str of the card type as BLANK
+
+# check_identifiers
+# # Check if card length and identifier digits are valid
+    # if false then exit() while printing("INVALID")
+
+    # else assign Identified card type (AMERICAN EXPRESS, MASTERCARD, VISA) to a variable
+
+# use re to get a list of the digits in the str
+
+# check_sum
+# # perform req math to check checksum iterating over the list while casting the chars to ints
+    # if the checksum is valid then print the card type variable and exit(0)
+
+    # else exit() and print ("INVALID")
+
+
+
 
 # import req modules
 from cs50 import get_int
 
 
-# get card number input
-CardNum = str(get_int("Number: "))
-
-# check if
-    # 1. card starting digits are correct
-    # 2. if card length is correct
+# get card number input and length
+CardNum = get_int("Number: ")
 CardLen = len(CardNum)
 
 
@@ -19,6 +37,7 @@ def main():
     global CardLen
 
     # it's messing up here because even if one is correct the other may not be so it returns invalid anyway
+    if check_card_len(13)
     check_card_len(13)
     check_card_len(16)
 
