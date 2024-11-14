9608612317b32562f8ed384f0f650c9e83167547
Tristan Stewart <tjstew880@gmail.com>
2024-11-14 17:31:26 +1100

    Added new tables (stocks, cash_transactions, stock_transactionsto) to the db. Added code to /quote & /register routes in app.py to access the newly created db tables as intended.

--- src/flask/problem set 9/lesson 1 - finance/app.py
+++ src/flask/problem set 9/lesson 1 - finance/app.py
@@ -35,6 +35,21 @@ def after_request(response):
 @login_required
 def index():
     """Show portfolio of stocks"""
+    # Complete the implementation of index in such a way that it displays an HTML table summarizing, for the user currently logged in,
+        # which stocks the user owns,
+
+        # the numbers of shares owned,
+
+        # the current price of each stock,
+
+        # and the total value of each holding (i.e., shares times price).
+
+    # Also display the user’s current cash balance along with a grand total (i.e., stocks’ total value plus cash).
+
+    # Odds are you’ll want to execute multiple SELECTs.
+        # Depending on how you implement your table(s), you might find GROUP BY HAVING SUM and/or WHERE of interest.
+
+    # Odds are you’ll want to call lookup for each stock.
     return apology("TODO")
 
 
@@ -42,6 +57,30 @@ def index():
 @login_required
 def buy():
     """Buy shares of stock"""
+    # Require that a user input a number of shares, implemented as a text field whose name is shares.
+        # Render an apology if the input is not a positive integer.
+
+    # Submit the user’s input via POST to /buy.
+
+    # Upon completion, redirect the user to the home page.
+
+    # Odds are you’ll want to call lookup to look up a stock’s current price.
+
+    # Odds are you’ll want to SELECT how much cash the user currently has in users.
+
+    # Add one or more new tables to finance.db via which to keep track of the purchase.
+        # Store enough information so that you know who bought what at what price and when.
+
+    # Use appropriate SQLite types.
+
+    # Define UNIQUE indexes on any fields that should be unique.
+
+    # Define (non-UNIQUE) indexes on any fields via which you will search (as via SELECT with WHERE).
+
+    # Render an apology, without completing a purchase, if the user cannot afford the number of shares at the current price.
+
+    # You don’t need to worry about race conditions (or use transactions).
+
     return apology("TODO")
 
 
@@ -49,6 +88,22 @@ def buy():
 @login_required
 def history():
     """Show history of transactions"""
+    # Complete the implementation of history in such a way that it displays an HTML table
+        # summarizing all of a user’s transactions ever,
+
+        # listing row by row each and every buy and every sell.
+
+    # For each row, make clear whether a stock was bought or sold and include
+        # the stock’s symbol,
+
+        # the (purchase or sale) price,
+
+        # the number of shares bought or sold,
+
+        # and the date and time at which the transaction occurred.
+
+    # You might need to alter the table you created for buy or supplement it with an additional table. Try to minimize redundancies.
+    
     return apology("TODO")
 
 
@@ -112,8 +167,13 @@ def quote():
             return redirect("/quote")
 
         results = lookup(symbol)
-        print(results)
-        print(type(results))
+
+        try:
+            db.execute("INSERT INTO stocks (name, symbol) VALUES (?, ?)", results["name"], results["symbol"])
+            print("stock profile added to db")
+        # might need to remove these lines below
+        except ValueError:
+            print("stock profile was not added to db")
 
         return render_template("quote.html", results=results, request=request)
 
@@ -144,6 +204,9 @@ def register():
         except ValueError:
             return apology("username already exists", 403)
 
+        user_id = db.execute("SELECT id FROM users WHERE username = ?", username)[0]["id"]
+        db.execute("INSERT INTO cash_transactions (user_id, date, description, amount, transaction_type) VALUES (?, CURRENT_TIMESTAMP, ?, ?, ?)", user_id, "Acc. Registration Bonus", 10000.00, "deposit")
+
         return redirect("/")
 
     else:
@@ -154,4 +217,18 @@ def register():
 @login_required
 def sell():
     """Sell shares of stock"""
+    # Complete the implementation of sell in such a way that it enables a user to sell shares of a stock (that he or she owns).
+
+    # Require that a user input a stock’s symbol, implemented as a select menu whose name is symbol.
+        # Render an apology if the user fails to select a stock or if (somehow, once submitted) the user does not own any shares of that stock.
+
+    # Require that a user input a number of shares, implemented as a text field whose name is shares.
+        # Render an apology if the input is not a positive integer or if the user does not own that many shares of the stock.
+
+    # Submit the user’s input via POST to /sell.
+
+    # Upon completion, redirect the user to the home page.
+
+    # You don’t need to worry about race conditions (or use transactions).
+
     return apology("TODO")
